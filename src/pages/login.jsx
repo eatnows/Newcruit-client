3bd9f6af4dfd35031ae37c87bc66cbcffb414cde
eatnows <gagyasindev@gmail.com>
2020-12-22 23:31:19 +0900

    faet: 구글 로그인 버튼 생성 #1 리액트 구글 로그인 모듈로 구글 로그인 버튼 생성 및 환경변수 파일 생성

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import REactDom from "react-dom";
+import GoogleLogin from "react-google-login";
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+const Login = () => {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+
+  const responseFail = (err) => {
+    console.error(err);
+  };
+
+  return (
+    <>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+    </>
+  );
+};
 
 export default Login;
