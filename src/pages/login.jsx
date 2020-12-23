3d882e418badfda72430b61590bb1eec782a21a1
eatnows <gagyasindev@gmail.com>
2020-12-24 00:50:43 +0900

    feat: aixos config 생성 #1 aixos 구성파일 생성

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,14 +1,29 @@
+import axios from "axios";
 import React from "react";
-import REactDom from "react-dom";
-import GoogleLogin from "react-google-login";
+import ReactDom from "react-dom";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 const Login = () => {
   const responseGoogle = (response) => {
     console.log(response);
+    const email = response.profileObj.email;
+    const name = response.profileObj.name;
+
+    const url = `${process.env.PUBLIC_URL}/login`;
+    axios.post(url, {
+      email: email,
+      name: name,
+    });
   };
 
   const responseFail = (err) => {
+    console.log("구글 로그인 실패");
+    console.log(err);
     console.error(err);
+  };
+
+  const logout = (res) => {
+    console.log(res);
   };
 
   return (
@@ -20,6 +35,11 @@ const Login = () => {
         onFailure={responseGoogle}
         cookiePolicy={"single_host_origin"}
       />
+      <GoogleLogout
+        clientId={process.env.REACT_APP_GOOGLE}
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      ></GoogleLogout>
     </>
   );
 };
